b58f18e6e90f6b450ffdee1abc90e422e908e1bc
Pedro Lopez <pedrolopezs@gmail.com>
2022-06-17 11:42:58 +0200

    V1.0

---
+++ docker_centos_init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <container_name>"
+  exit 1
+fi
+docker network create lab &>/dev/null
+docker container run -dti --net=lab --tmpfs /run --tmpfs /tmp -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name "$1" --privileged server1:5000/centos-init &>/dev/null
+echo "$(docker container inspect -f '{{ .NetworkSettings.Networks.lab.IPAddress }}' $1) ansible_host=$1 ansible_connection=docker"
---
+++ mkwebdir.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+if [[ "${#}" -ne 1 ]]; then
+	echo "Create a simple web directory for <username> along with a generic index.html"
+	echo "Usage: ${0} <username>"
+	exit 2
+fi
+
+USER="${1}"
+
+if [[ -z "${HOMEDIR}" ]]; then
+	useradd -m $USER
+fi
+
+HOMEDIR="$(getent passwd "${USER}" | cut -d: -f6)"
+PUBDIR="${HOMEDIR}/public_html"
+
+if [[ ! -d "${HOMEDIR}" ]]; then
+	echo "Error: The directory ${HOMEDIR} doesn't exist."
+	exit 1
+fi
+
+mkdir "${PUBDIR}"
+if [[ -d "${PUBDIR}" ]]; then
+	cat << 'EOF' > "${PUBDIR}/index.html"
+<!DOCTYPE html>
+<html>
+<head>
+	<title>World! Hello?</title>
+</head>
+	<body>
+		<h1>Hello World</h1>
+	</body>
+</html>
+EOF
+	chown -R "${USER}:" "${PUBDIR}"
+	chmod 755 "${HOMEDIR}"
+else
+	echo "Error: failed to create directory ${PUBDIR}."
+	exit 1
+fi
