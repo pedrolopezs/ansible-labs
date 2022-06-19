204a0be79be7e548e7d100f62e98a3c293970f33
Cloud User <centos@ip-172-31-2-176.us-west-2.compute.internal>
2022-06-19 07:37:07 +0000

    v1.9

--- install_docker.sh
+++ install_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ansible pls -m yum -a "name=docker state=present"
-ansible pls -m user -a "name=centos groups=dockerroot,adm,systemd-journal state=present"
+ansible pls -m file -a "name=/etc/docker state=directory"
+ansible pls -m copy -a "dest=/etc/docker/daemon.json src=daemon.json"
 ansible pls -m systemd -a "name=docker enabled=yes state=started"
 ansible pls  -a "docker version" 
 
